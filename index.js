f0c92e47c3c7177d11a71f03f1950fa36db943ca
tejasrinainala <tejasrinainala@gmail.com>
2024-05-21 19:43:07 +0530

    Added js file

---
+++ index.js
@@ -0,0 +1,25 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sub: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Div: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Product: " + result
+}
